f4a0d41d110214ddd0b04b5141e41e948b6e35a7
l00196750 <liyangqi@gmail.com>
2017-08-15 11:21:10 +0800

    commit in eclipse

--- test/hys/timetable/FloatTest.java
+++ test/hys/timetable/FloatTest.java
@@ -6,16 +6,16 @@ import com.hys.common.utils.Loggers;
 
 public class FloatTest {
 
-    @Test
-    public void t1() {
-        float count = 1.5f;
-        int i1 = 3;
-
-        float avg = 1.0f * i1 / 2;
-        Loggers.log.debug("{}", i1 > count);
-        Loggers.log.debug("{}", count > i1);
-        Loggers.log.debug("{}", avg);
-        Loggers.log.debug("{}", Math.round(2.1f));
-        Loggers.log.debug("{}", Double.valueOf(Math.ceil(2.1f)).intValue());
-    }
+	@Test
+	public void t1() {
+		float count = 1.5f;
+		int i1 = 3;
+		float avg = 1.0f * i1 / 2;
+		Loggers.log.debug("");
+		Loggers.log.debug("{}", i1 > count);
+		Loggers.log.debug("{}", count > i1);
+		Loggers.log.debug("{}", avg);
+		Loggers.log.debug("{}", Math.round(2.1f));
+		Loggers.log.debug("{}", Double.valueOf(Math.ceil(2.1f)).intValue());
+	}
 }
